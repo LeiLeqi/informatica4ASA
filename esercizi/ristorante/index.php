e6e49b92f9962f917037a47960222e29535ab62d
LeiLeqi <147245657+LeiLeqi@users.noreply.github.com>
2023-11-18 11:58:59 +0000

    s

--- esercizi/ristorante/index.php
+++ esercizi/ristorante/index.php
@@ -1,41 +1,41 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Catalogo Ristorante -  La patatona</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        body {
-            background-image: url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wineandfoodtour.it%2Fpatate-al-forno-cosi-buone-e-sfiziose-non-le-hai-mai-provate-finiranno-in-3-minuti%2F&psig=AOvVaw0-W3QjeyoR5rvUv0MTrvNT&ust=1699788937903000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCOjz0bjtu4IDFQAAAAAdAAAAABAE;
-            background-size: cover;
-            padding-top: 20px;
-        }
-        header{
-            text-align: center;
-            margin-bottom: 20px;
-        }
-        header h1{
-            font-size: 56px;
-            color: rgb(238, 227, 132);
-        }
-        header h2{
-            font-size: 24px;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Menu Ristorante</title>
 </head>
-<body>
-    <header>
-        <h1>TRATTORIA LA PATATA</h1>
-        <h2 style="text-decoration: underline;">I nostri prodotti gourmet</h2>
-    </header>
-    <nav>
-        <ul>
-            <?php
+<body> 
+    <div class="container mt-5">
+        <h2>Menu Ristorante PatatePerTutti</h2>
 
-                require_once 'include/config.php';
-                require_once '.php';
-                $patate = array('Patata ripiena', 'Patate al forno', 'Patate fritte', 'Crocchette di patate', 'Pizzette di patate');
-            ?>
-        </ul>
-    </nav>
-        
+        <?php
+  
+            $piattiPatate = array(
+            array("nome" => "Gateau di Patate", "prezzo" => 14.99, "immagine" => "https://www.giallozafferano.it/images/165-16591/Gateau-di-patate_450x300.jpg"),
+            array("nome" => "Parmigiana di patate", "prezzo" => 19.99, "immagine" => "https://www.giallozafferano.it/images/229-22972/Parmigiana-di-patate_360x300.jpg"),
+            array("nome" => "Patate ripiene", "prezzo" => 9.99, "immagine" => "https://cdn.cook.stbm.it/thumbnails/ricette/144/144690/hd400x225.jpg"),
+            array("nome" => "Frico Friuliano", "prezzo" => 7.99, "immagine" => "https://www.ricettedellanonna.net/wp-content/uploads/2015/02/frico-friulano-foto-620x414.jpg"),        
+            );
+
+            foreach ($piattiPatate as $piatto) {
+                echo '
+                <div class="col-md-6" style="width: 80rem";>
+                    <img src="' . $piatto["immagine"] . '" class="card-img-top" alt="' . $piatto["nome"] . '">
+                    <div class="card-body">
+                        <h5 class="card-title">' . $piatto["nome"] . '</h5>
+                        <p class="card-text">Prezzo: $' . $piatto["prezzo"] . '</p>
+                        <a href="#" class="btn btn-primary">Aggiungi al carrello</a>
+                    </div>
+                </div>';
+            }
+        ?>
+    </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
 </html>
